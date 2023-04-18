71ef01dcb3d85a2bfaaafdc8e4625b8a9db28dc4
Michal Gajlewicz <gajlewicz.m@gmail.com>
2023-04-18 07:26:07 +0200

    3rd commit: black

--- main.py
+++ main.py
@@ -2,42 +2,39 @@ import glob
 import cv2
 from PIL import Image
 
+
 class Gif:
-    TEMP_DIR = 'tmp/'
+    TEMP_DIR = "tmp/"
+
     def __init__(self, path: str):
         self.movie = cv2.VideoCapture(path)
 
     def read_frame(self, time: int):
         self.movie.set(cv2.CAP_PROP_POS_MSEC, time)
         return self.movie.read()
-        
+
     def _get_frames(self, fps):
         time = 1
         success, frame = self.read_frame(time)
-        
+
         while success:
-            cv2.imwrite(f'{Gif.TEMP_DIR}{time}.jpg', frame)
+            cv2.imwrite(f"{Gif.TEMP_DIR}{time}.jpg", frame)
             success, frame = self.read_frame(round(time / fps * 1000))
             time += 1
-            
-        
+
     def save(self, target: str, fps):
         self._get_frames(fps)
-        images = glob.glob(f'{Gif.TEMP_DIR}*.jpg')
+        images = glob.glob(f"{Gif.TEMP_DIR}*.jpg")
         frames = [Image.open(image) for image in images]
         frames[0].save(
-            target, 
-            format='GIF',
+            target,
+            format="GIF",
             append_images=frames,
             save_all=True,
             duration=100,
-            loop=5
+            loop=5,
         )
-       
-            
-            
 
 
-    
-gif = Gif('movie.mp4')
-gif.save('movie.gif', 1)
+gif = Gif("movie.mp4")
+gif.save("movie.gif", 1)
